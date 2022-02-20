6e391a6ed66204c6171b76d44de271cd95f8929e
ananias18 <euananias@gmail.com>
2022-02-20 15:47:28 -0300

    Streams

---
+++ File_AND_FileINFO/File_AND_FileINFO/Program.cs
@@ -0,0 +1,71 @@
+﻿CriarDiretoriosGlobo();
+CriarArquivo();
+
+var origem = Path.Combine(Environment.CurrentDirectory, "brasil.txt");
+var destino = Path.Combine(Environment.CurrentDirectory, "globo", "America do Sul", "Brasil", "brasil.txt");
+
+MoverArquivo(origem, destino);
+CopiarArquivo(origem, destino);
+
+static void CopiarArquivo(string pathOrigem, string pathDestino)
+{
+    File.Copy(pathOrigem, pathDestino);
+}
+
+static void MoverArquivo(string pathOrigem, string pathDestino)
+{
+    if (!File.Exists(pathOrigem))
+    {
+        Console.WriteLine("Arquivo de origem não existe");
+        return;
+    }
+    if (File.Exists(pathDestino))
+    {
+        Console.WriteLine("Arquivo de destino já existe");
+        return;
+    }
+
+    File.Move(pathOrigem, pathDestino);
+}
+
+static void CriarArquivo()
+{
+    var path = Path.Combine(Environment.CurrentDirectory, "brasil.txt");
+    if (!File.Exists(path))
+    {
+        using var sw = File.CreateText(path);
+        sw.WriteLine("População: 213MM");
+        sw.WriteLine("IDH: 0,901");
+        sw.WriteLine("Dados Atualizados em: 20/04/1985");
+    }
+}
+static void CriarDiretoriosGlobo()
+{
+
+    var path = Path.Combine(Environment.CurrentDirectory, "globo");
+    if (!Directory.Exists(path))
+    {
+        var dirGlobo = Directory.CreateDirectory(path);
+        var dirAmNorte = dirGlobo.CreateSubdirectory("america do norte");
+        var dirAmCentral = dirGlobo.CreateSubdirectory("america do Central");
+        var dirAmSul = dirGlobo.CreateSubdirectory("america do Sul");
+
+        dirAmNorte.CreateSubdirectory("USA");
+        dirAmNorte.CreateSubdirectory("Mexico");
+        dirAmNorte.CreateSubdirectory("Canada");
+
+        dirAmCentral.CreateSubdirectory("Costa rica");
+        dirAmCentral.CreateSubdirectory("Panama");
+
+        dirAmSul.CreateSubdirectory("Brasil");
+        dirAmSul.CreateSubdirectory("Argentina");
+        dirAmSul.CreateSubdirectory("Paraguai");
+
+    }
+}
+
+
+
+
+
+
---
+++ Streams_em_C#/Program.cs
@@ -0,0 +1,42 @@
+﻿using static System.Console;
+
+WriteLine("digite o nome do aquivo:");
+
+var nome = ReadLine();
+
+nome = LimparNome(nome);
+
+foreach (var @char in Path.GetInvalidFileNameChars())
+{
+    nome = nome.Replace(@char, '-');
+}
+var path = Path.Combine(Environment.CurrentDirectory, $"{nome}.txt");
+
+CriarArquivo(path);
+
+WriteLine("Digite enter para finalizar");
+ReadLine();
+
+static string LimparNome(string nome)
+{
+    foreach (var @char in Path.GetInvalidFileNameChars())
+{
+    nome = nome.Replace(@char, '-');
+}
+return nome;
+}
+static void CriarArquivo(string path)
+{
+    try
+    {
+        using var sw = File.CreateText(path);
+        sw.WriteLine("escreva aqui 1");
+        sw.WriteLine("escreva aqui 2");
+        sw.WriteLine("escreva aqui 3");
+
+    }
+    catch
+    {
+        WriteLine("O nome do arquivo esta invalido!");
+    }
+}
